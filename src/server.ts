5dedf7c60462546c2399204181214897c9b2ef00
Alex Lillo <alexlillo26@gmail.com>
2025-04-08 20:26:38 +0200

    Arreglat per tal que surtin les rutes

--- src/server.ts
+++ src/server.ts
@@ -92,7 +92,7 @@ app.get('/', (req, res) => {
 // Conexión a MongoDB
 //mongoose;
 mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb+srv://joan:1234@cluster0.3owhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/seminarijwt')
     .then(() => console.log('Connected to DB'))
     .catch((error) => console.error('DB Connection Error:', error));
 
